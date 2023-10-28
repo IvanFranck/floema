159bdd6c1dc99644720b66c55b24f6fc155e8eb7
ivan nzima <nzimaivan007@gmail.com>
2023-10-28 05:34:40 +0100

    fetching collection page cotent form the CMS

--- app.js
+++ app.js
@@ -14,30 +14,39 @@ app.use((req, res, next) => {
   res.locals.ctx = {
     prismic
   }
+  res.locals.Numbers = index => {
+    return index === 0 ? 'One' : index === 1 ? 'Two' : index === 2 ? 'Three' : index === 3 ? 'Four' : ''
+  }
   next()
 })
 
 app.set('views', path.join(currentDirectory, 'views'))
 app.set('view engine', 'pug')
 
-
-app.get('*', async (req, res, next) => {
-  const meta = await client.getSingle('meta')
-  res.locals.meta = { ...meta }
-  next()
-})
-
 app.get('/', async (req, res) => {
-  res.render('pages/home')
+  const meta = await client.getSingle('meta')
+  const preloader = await client.getSingle('preloader')
+  res.render('pages/home', { preloader, meta })
 })
 
 app.get('/about', async (req, res) => {
+  const meta = await client.getSingle('meta')
   const about = await client.getSingle('about')
-  res.render('pages/about', { about })
+  const preloader = await client.getSingle('preloader')
+  res.render('pages/about', { about, preloader, meta })
 })
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
+app.get('/collections', async (req, res) => {
+  const home = await client.getSingle('home')
+  const preloader = await client.getSingle('preloader')
+  const meta = await client.getSingle('meta')
+
+  const collections = await client.getAllByType('collection', {
+    fetchLinks: 'product.image'
+  })
+  // console.log("preloader", preloader)
+  // console.log(collections[0].data)
+  res.render('pages/collections', { collections, home, preloader, meta })
 })
 
 app.get('/detail/:uid', async (req, res) => {
@@ -45,8 +54,9 @@ app.get('/detail/:uid', async (req, res) => {
   const product = await client.getByUID('product', uid, {
     fetchLinks: 'collection.title'
   })
-  console.log("product:", product.data.informations)
-  res.render('pages/detail', { product })
+  const meta = await client.getSingle('meta')
+  const preloader = await client.getSingle('preloader')
+  res.render('pages/detail', { product, preloader, meta })
 })
 
 app.listen(port, () => {
