d3aeca808a7f82e6b6bdc121df37932f1e7e59a2
ivan nzima <nzimaivan007@gmail.com>
2023-11-09 01:39:15 +0100

    set up scss file architecture and code base

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(errorHandler())
+app.use(express.static(path.join(currentDirectory, 'public')))
 
 const handleLinkResolver = (doc) => {
   if (doc.type === 'product') {
